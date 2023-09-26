4dc610b2a20b5c8960f72ab534a54362d8631937
Ashwani <ashwani.kumar@codmsoftware.com>
2023-09-26 14:03:40 +0530

    fixde image and manuf page

--- Products/clm-contract-lifecycle-management.php
+++ Products/clm-contract-lifecycle-management.php
@@ -2,7 +2,7 @@
 <?php include "../app/header.php"; ?>
 
 
-<section id="banner" style="background-image:url(../assect/upload/Products/clm-contract-lifecycle-management\ .jpg);">
+<section id="banner" style="background-image:url(../assect/upload/Products/clm-contract-lifecycle-management.jpg);">
     <h3 class="line text-white">Streamline CLM with Easy Contract Lifecycle Management Solutions</h3>
 </section>
 <section class="mt-5">
