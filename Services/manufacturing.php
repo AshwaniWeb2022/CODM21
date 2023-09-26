8d755c34af982f9b7d614b812c4c8d6ae14328cb
Ashwani <ashwani.kumar@codmsoftware.com>
2023-09-26 15:30:10 +0530

    manufac agen send

--- Services/manufacturing.php
+++ Services/manufacturing.php
@@ -1,7 +1,7 @@
 <?php
-include "../client/app/header.php";
+include "../app/header.php";
 ?>
-<section id="banner" style="background-image:url(../client//imageFoleder/Digital-Manufacturing-Cloud.webp);">
+<section id="banner" style="background-image:url(../assect/upload/Service/Digital-Manufacturing-Cloud.jpg);">
     <h3 class="line text-white">Manufacturing cloud</h3>
 </section>
 
@@ -10,63 +10,56 @@ include "../client/app/header.php";
         <div class="row">
             <div class="col-lg-4 align-self-center mb-2">
                 <h2 class="pt-2 heading_titel">The next generation of manufacturing is here
-Photo of men with astro in a manufacturing environment</h2>
+                    Photo of men with astro in a manufacturing environment</h2>
                 <p class="pt-1 heading_p">Salesforce Manufacturing Cloud is a new solution designed specifically for manufacturers to unify account planning and forecasting, providing greater visibility and collaboration across their ecosystem.</p>
 
             </div>
             <div class="col-lg-8 align-self-center mb-2">
-                <img src="../client/imageFoleder/manufacturing-cloud-thumb-1.webp" alt="image" style="width:100%; height:380px;">
+                <img src="../assect/upload/Service/manufacturing-cloud-thumb-1.jpg" alt="image" style="width:100%; height:380px;">
             </div>
             <div class="col-lg-12 mt-2">
                 <p class="pt-1 heading_p">Manufacturing is a critical cog in the global economy. In the U.S., it employs over 12 million people and contributes about $2 trillion to GDP. Globally, output in the 34 largest manufacturing regions totals over $35 trillion. This sector is also on the cusp of new productivity, efficiency and innovation as technology ushers in the next generation of manufacturing based on a connected ecosystem and smart analytics.
 
-We intend to drive this transformation with Salesforce Manufacturing Cloud, a new solution designed specifically for manufacturers to unify account planning and forecasting, providing greater visibility and collaboration across their ecosystem. This unified view provides a clear picture of manufacturing customers through new sales agreements and account-based forecasting solutions that enable them to accurately forecast, plan and deliver predictable (and better) business results.</p>
+                    We intend to drive this transformation with Salesforce Manufacturing Cloud, a new solution designed specifically for manufacturers to unify account planning and forecasting, providing greater visibility and collaboration across their ecosystem. This unified view provides a clear picture of manufacturing customers through new sales agreements and account-based forecasting solutions that enable them to accurately forecast, plan and deliver predictable (and better) business results.</p>
             </div>
-              <div class="col-lg-12 mt-2">
-                  <h2 class="pt-2 heading_titel">Helping manufacturers align sales and operations</h2>
-                  <p class="pt-1 heading_p">Success in the manufacturing industry depends on great products and meeting customer demand through efficient production and fulfillment of those products. The first step in this process is to ensure that customer teams have an overview of a customer's entire business scope. This includes ongoing business, run-rate business and new opportunities. This visibility leads to more accurate forecasting, more predictable business, and ultimately more revenue.
-<br>
-<br>
-Predictability is paramount for all stakeholders in the production system. Customers want certainty in pricing, availability and delivery; distributors want optimized delivery channels; sales teams want pricing based on real-time volume assumptions; and operations teams want to produce and procure components based on the most accurate demand possible.
+            <div class="col-lg-12 mt-2">
+                <h2 class="pt-2 heading_titel">Helping manufacturers align sales and operations</h2>
+                <p class="pt-1 heading_p">Success in the manufacturing industry depends on great products and meeting customer demand through efficient production and fulfillment of those products. The first step in this process is to ensure that customer teams have an overview of a customer's entire business scope. This includes ongoing business, run-rate business and new opportunities. This visibility leads to more accurate forecasting, more predictable business, and ultimately more revenue.
+                    <br>
+                    <br>
+                    Predictability is paramount for all stakeholders in the production system. Customers want certainty in pricing, availability and delivery; distributors want optimized delivery channels; sales teams want pricing based on real-time volume assumptions; and operations teams want to produce and procure components based on the most accurate demand possible.
 
-<br>
-<br>
-To achieve this, manufacturers need a clear understanding of all past shipments, current production plans, and future production forecasts. The challenge most face is information silos across multiple systems, including CRM, ERP, and supply chain planning systems. On top of that, there are usually various teams involved, which bogs down the process and makes the sales and operations process murky at best. Perhaps worst of all, sales and distribution partners — those closest to the customer — are not accurately represented!
-<br>
-<br>
-Given the current manufacturing ecosystem, there is now a golden opportunity to link customers, distribution partners, and sales teams to change the operational planning paradigm. The next generation of manufacturing leaders will establish a new level of connection and engagement with their customers. Achieving this requires universal access to previously siloed data and the ability to extract insights from that data. One manufacturer recently told me that these advancements portend a shift from acting on the point of view of the customer to the actual needs of the customer.
-<br>
-<br>
-</p>
+                    <br>
+                    <br>
+                    To achieve this, manufacturers need a clear understanding of all past shipments, current production plans, and future production forecasts. The challenge most face is information silos across multiple systems, including CRM, ERP, and supply chain planning systems. On top of that, there are usually various teams involved, which bogs down the process and makes the sales and operations process murky at best. Perhaps worst of all, sales and distribution partners — those closest to the customer — are not accurately represented!
+                    <br>
+                    <br>
+                    Given the current manufacturing ecosystem, there is now a golden opportunity to link customers, distribution partners, and sales teams to change the operational planning paradigm. The next generation of manufacturing leaders will establish a new level of connection and engagement with their customers. Achieving this requires universal access to previously siloed data and the ability to extract insights from that data. One manufacturer recently told me that these advancements portend a shift from acting on the point of view of the customer to the actual needs of the customer.
+                    <br>
+                    <br>
+                </p>
 
-<h2 class="pt-2 heading_titel">How Manufacturing Cloud works</h2>
-<p class="pt-1 heading_p">
-    We introduced Manufacturing Cloud to streamline the alignment between sales and operations, revamp the sales forecasting process for both ongoing business and new opportunities, and extend the planning collaboration to our partners. Here's a breakdown of how it works.
-    <br>
-    <br>
-    Sales agreements provide manufacturers with the means to consolidate ongoing business data from ERP and OMS systems alongside negotiated contract terms, encompassing planned volumes and revenue. This ensures that both our operations and account teams have a comprehensive 360-degree view of our customers. Any modifications are automatically integrated into the sales agreement, ensuring a single, authoritative source of information. Our account teams now possess the capability to oversee the complete sales agreement lifecycle, gaining insights into committed and actual order volumes. Additionally, they can monitor the agreement's performance against the forecast and other customized time-phased metrics.
-<br>
-<br>
-Our account-based forecasting approach fosters collaboration across the entire organization, breaking down internal barriers that have traditionally hindered accurate forecasting. This empowers our sales, finance, and operations teams to generate more precise forecasts. Our account teams can also contribute updates regarding evolving customer needs and broader market trends, allowing us to adapt plans and estimates in real-time, thereby enhancing the predictability of our business transactions, profits, and revenues.
-<br>
-<br>
-Many manufacturers distribute their products through a network of distributors and dealers who possess valuable insights into changing customer preferences and market demands. To harness this valuable knowledge, we've integrated them into the forecast collaboration process through a pre-configured community template.
-<br>
-<br>
-This groundbreaking capability equips manufacturers with unparalleled access to real-time insights concerning customer behavior, product performance, and pricing dynamics, enabling swift translation of these insights into actionable strategies.
-<br>
-<br>
-<i><b>This adaptability exemplifies the core principles of an agile organization."</b></i>
-</p>
-                  </div>
+                <h2 class="pt-2 heading_titel">How Manufacturing Cloud works</h2>
+                <p class="pt-1 heading_p">
+                    We introduced Manufacturing Cloud to streamline the alignment between sales and operations, revamp the sales forecasting process for both ongoing business and new opportunities, and extend the planning collaboration to our partners. Here's a breakdown of how it works.
+                    <br>
+                    <br>
+                    Sales agreements provide manufacturers with the means to consolidate ongoing business data from ERP and OMS systems alongside negotiated contract terms, encompassing planned volumes and revenue. This ensures that both our operations and account teams have a comprehensive 360-degree view of our customers. Any modifications are automatically integrated into the sales agreement, ensuring a single, authoritative source of information. Our account teams now possess the capability to oversee the complete sales agreement lifecycle, gaining insights into committed and actual order volumes. Additionally, they can monitor the agreement's performance against the forecast and other customized time-phased metrics.
+                    <br>
+                    <br>
+                    Our account-based forecasting approach fosters collaboration across the entire organization, breaking down internal barriers that have traditionally hindered accurate forecasting. This empowers our sales, finance, and operations teams to generate more precise forecasts. Our account teams can also contribute updates regarding evolving customer needs and broader market trends, allowing us to adapt plans and estimates in real-time, thereby enhancing the predictability of our business transactions, profits, and revenues.
+                    <br>
+                    <br>
+                    Many manufacturers distribute their products through a network of distributors and dealers who possess valuable insights into changing customer preferences and market demands. To harness this valuable knowledge, we've integrated them into the forecast collaboration process through a pre-configured community template.
+                    <br>
+                    <br>
+                    This groundbreaking capability equips manufacturers with unparalleled access to real-time insights concerning customer behavior, product performance, and pricing dynamics, enabling swift translation of these insights into actionable strategies.
+                    <br>
+                    <br>
+                    <i><b>This adaptability exemplifies the core principles of an agile organization."</b></i>
+                </p>
+            </div>
         </div>
     </div>
 </section>
-
-
-
-
-
-<!-- footer include  -->
-<?php include "../client/app/footer.php"; ?>
-<!-- footer include END -->
+<?php include "../app/footer.php";?>
